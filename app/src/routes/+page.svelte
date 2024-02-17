681d905590cf3a38a170da745eb66e60e08a29df
Atul V Chandran <atulvc2001@gmail.com>
2024-02-17 20:31:25 +0530

    feat: added css to main welcome page

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to UVA Meridian Employee detail generator</h1>
+<div class="main__body">
+    <h1>Welcome to UVA Meridian Employee detail generator</h1>
+</div>
 
+<style>
+    *{
+        color:#673052;
+        font-family: arial;
+        font-weight: 800;
+    }
+    .main__body{
+        display: flex;
+        justify-content: center;
+    }
+</style>
