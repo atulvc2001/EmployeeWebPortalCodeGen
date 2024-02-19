dfe0e5ed9adad227af08a5e9b200e96ad24c7f19
Atul V Chandran <atulvc2001@gmail.com>
2024-02-19 20:46:56 +0530

    images are getting downloaded and displayed, but need to find a way to remove the image and refactor code once more

--- app/src/routes/fetchMemberData/download/[dispEmpId]/+page.svelte
+++ app/src/routes/fetchMemberData/download/[dispEmpId]/+page.svelte
@@ -1,21 +1,43 @@
 <script>
+	import { onMount } from "svelte"
 	export let data;
 	const { dispEmpId } = data
 	const URLslug = `https://clubmembership.uvameridian.com/fetchMemberData/${dispEmpId}` 
 	console.log(URLslug)
 	import lion from '$lib/images/lion.jpg';
-
+	let showImage = false
 	// The client side solution would only work if the requested image is NOT blocked by CORS policy.
 
+
+	// function to download image and append image to the div block
 	async function download(url,qrformats) {
 		const a = document.createElement('a');
-		a.href = await toDataURL(url);
+		const img = document.createElement("img")
+		a.href = await toDataURL(url);	
+		console.log(a)
+
+		// To display image 
+		img.src = await toDataURL(url)
+		const divBlock = document.getElementsByClassName("image")[0]
+		if(divBlock.getElementsByTagName('img')[0]){
+			divBlock.removeChild(img)
+		} else {
+			// appending image if the child img tag doesn't exist
+			divBlock.appendChild(img)
+			const show_img = document.getElementsByClassName("img__container")[0]
+			show_img.classList.add("show__img")
+		}
+
+
+
+		// download image section
 		a.download = `myImage.${qrformats}`;
 		document.body.appendChild(a);
 		a.click();
 		document.body.removeChild(a);
 	}
 
+	// converting the api data to image with blob
 	function toDataURL(url) {
 		return fetch(url)
 			.then((response) => {
@@ -39,6 +61,9 @@
 			`https://api.qrserver.com/v1/create-qr-code/?data=${URLslug}&amp;size=600x600&format=${qrformats}`,qrformats
 		);
 		
+	onMount(() => {
+		showImage = true;
+	})
 
 	}
 
@@ -46,15 +71,20 @@
 
 <body>
 	<div class="container">
-		<form on:submit={handlesubmit} action="">
-			<h1>Download QR Code</h1>
-			<select name="qrformats" id="qrformat">
-				<option value="png">png</option>
-				<option value="jpeg">jpeg</option>
-				<option value="eps">eps</option>
-			</select>
-			<button>download</button>
-		</form>
+		<div class="container__text">
+			<form on:submit={handlesubmit} action="">
+				<h1>Download QR Code</h1>
+				<select name="qrformats" id="qrformat">
+					<option value="png">png</option>
+					<option value="jpeg">jpeg</option>
+					<option value="eps">eps</option>
+				</select>
+				<button>Display Image</button>
+			</form>
+		</div>
+		<div class="img__container" class:show__img={showImage} >
+			<div class="image"></div>
+		</div>
 	</div>
 
 </body>
@@ -64,12 +94,18 @@
 		font-family: Arial, Helvetica, sans-serif;
 	}
 
+	body{
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
 	.container {
 		border: 5px solid;
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		transform: translate(-50%, -100%);
 		padding: 30px;
+	}
+	
+	.show__img {
+		margin-top: 30px;
 	}
 </style>
