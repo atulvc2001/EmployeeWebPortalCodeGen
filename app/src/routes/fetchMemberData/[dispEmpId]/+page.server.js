b6b656191d10e88213d6a7d40fde98c1c96c51d4
Atul V Chandran <atulvc2001@gmail.com>
2024-02-22 20:34:35 +0530

    refactored code

--- app/src/routes/fetchMemberData/[dispEmpId]/+page.server.js
+++ app/src/routes/fetchMemberData/[dispEmpId]/+page.server.js
@@ -1,5 +1,5 @@
 
-// import { error } from '@sveltejs/kit';
+import { error } from '@sveltejs/kit';
 
 export const load = async (serverLoadEvent) => {
 	const { fetch, params } = serverLoadEvent;
@@ -8,29 +8,32 @@ export const load = async (serverLoadEvent) => {
 	// fetching Data from google sheet
 	const response = await fetch(
 		// google script link
-		'https://script.google.com/macros/s/AKfycbwa7GJFG2c-O6748gXfEKHIw3won1RxoK9bRymvEwvmXgwhz8z2ZA_zOoQHGZEZ-O-t/exec'
+		'https://script.google.com/macros/s/AKfycbwL0KrTv1rcvRWtPx_7MMtYVPUUOFEM5vCrw3bw0Xrd3WUxEQFG4ppnnI3JPZyI_5vL/exec'
 	);
 	const dataFromGoogleSheet = await response.json();
 	const gsheetEmpDataJson = { empsData: dataFromGoogleSheet };
 	// gsheetEmpDataJson.empsData.push(dataFromGoogleSheet)
 	console.log('This page is from +page.server.js');
-	// console.log(gsheetEmpDataJson.empsData) Test
-	console.log("hello") 
+	console.log(gsheetEmpDataJson.empsData)
+
+	// gsheetEmpDataJson.empsData.map((emp) => {
+	// 	console.log(emp.uid)
+	// })
 
 	// filtering the particular empId from the params
 	const singleEmpData = gsheetEmpDataJson.empsData.filter((emp) => {
-		// return emp.uid == dispEmpId;
-		return emp.Timestamp == dispEmpId;
+		return emp.uid == dispEmpId;
+		// return emp.Timestamp == dispEmpId;
 
 	});
 
-	console.log(singleEmpData)
+	console.log(singleEmpData[0]["uid"].length)
 
-	// if (!(singleEmpData.length == 9)){
-	// 	error(404, {
-	// 			message: 'ID Not found'
-	// 	});
-	// }
+	if (!(singleEmpData[0]["uid"].length == 9)){
+		error(404, {
+				message: 'ID Not found'
+		});
+	}
 	// sending the data to the html page
 	return {
 		singleEmpData,
