2dc18eaa6c4768476ea8d7abc38d5f694060ea0e
Atul V Chandran <atulvc2001@gmail.com>
2024-04-28 20:15:23 +0530

    updated downloads page

--- app/src/routes/fetchMemberData/download/[dispEmpId]/+page.server.js
+++ app/src/routes/fetchMemberData/download/[dispEmpId]/+page.server.js
@@ -5,11 +5,12 @@ export const load = async (serverLoadEvent) => {
     const { params } = await serverLoadEvent;
     const { dispEmpId } = await params;
 
-    if (dispEmpId.length != 9){
-		error(404, {
-				message: 'ID Not found'
-		});
-	}
+    // ! rectify uid identifier
+    // if (dispEmpId.length != 9){
+	// 	error(404, {
+	// 			message: 'ID Not found'
+	// 	});
+	// }
 
     console.log(dispEmpId.length)
 
